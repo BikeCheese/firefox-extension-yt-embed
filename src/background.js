441d9c67c372f1a35feac0821fde2c51a0a05345
BikeCheese <152095922+BikeCheese@users.noreply.github.com>
2023-12-10 12:32:13 -0800

    Improvements And Bug Fix
    Improving the implementation for extracting the query string parameter for the video id or the playlist id from the url. In doing so, also fixing the bug where the extension fails when a given youtube url has a timestamp in the query string.

--- src/background.js
+++ src/background.js
@@ -5,25 +5,73 @@ browser.menus.create({
 });
 
 browser.menus.onClicked.addListener(async function (info, tab) {
-
-    if (info.menuItemId == "ff-ext-yt-embed") {
+    
+    if (extensionMenuItemClicked(info.menuItemId)) {
         if (info.linkUrl) {
-            let embedPageUrl;
-            if (info.linkUrl.includes("list=")) {
-                let playlistIdStartingIndex = info.linkUrl.indexOf("list=") + 5;
-                let playlistId = info.linkUrl.substring(playlistIdStartingIndex);
-                embedPageUrl = "https://www.youtube.com/embed/videoseries?list=" + playlistId;
-            } else {
-                let videoIdStartingIndex = info.linkUrl.indexOf("v=") + 2;
-                let videoId = info.linkUrl.substring(videoIdStartingIndex);
-                if (videoId.includes("pp=")) {
-                    videoId = videoId.substring(0, videoId.indexOf("&pp="));
+            if (isYoutubeLink(info.linkUrl)) {
+                let embedPageUrl;
+                let youtubeUrlType = determineYoutubeUrlType(info.linkUrl);
+                if (youtubeUrlType == "VIDEO") {
+                    let videoId = findVideoId(info.linkUrl);
+                    embedPageUrl = "https://www.youtube.com/embed/" + videoId;
+                } else if (youtubeUrlType == "PLAYLIST") {
+                    let playlistId = findPlaylistId(info.linkUrl);
+                    embedPageUrl = "https://www.youtube.com/embed/videoseries?list=" + playlistId;
                 }
-                embedPageUrl = "https://www.youtube.com/embed/" + videoId
+                if (embedPageUrl) {
+                    await browser.tabs.create({
+                        'active': false, 'url': embedPageUrl, 'index': tab.index + 1
+                    });
+                }
             }
-            await browser.tabs.create({
-                'active': false, 'url': embedPageUrl, 'index': tab.index + 1
-            });
         }
     }
 });
+
+function extensionMenuItemClicked(menuItemId) {
+    
+    return menuItemId == "ff-ext-yt-embed";
+}
+
+function isYoutubeLink(url) {
+    
+    return url.includes("youtube.com");
+}
+
+function determineYoutubeUrlType(url) {
+    
+    let urlType = "UNKNOWN";
+
+    if (url.includes("list=")) {
+        urlType = "PLAYLIST";
+    } else if (url.includes("v=")) {
+        urlType = "VIDEO";
+    }
+
+    return urlType;
+}
+
+function findVideoId(url) {
+    
+    return getQueryStringParameter(url,"v");
+}
+
+function findPlaylistId(url) {
+    
+    return getQueryStringParameter(url,"list");
+}
+
+function getQueryStringParameter(url, queryStringParameterId) {
+    
+    let queryStringParameterValue;
+    let queryStringParameterStartingIndex = url.indexOf(queryStringParameterId + "=") + queryStringParameterId.length + 1;
+    let nextAmpersandIndex = url.substring(queryStringParameterStartingIndex).indexOf("&");
+
+    if (nextAmpersandIndex == -1) {
+        queryStringParameterValue = url.substring(queryStringParameterStartingIndex);
+    } else {
+        queryStringParameterValue = url.substr(queryStringParameterStartingIndex, nextAmpersandIndex);
+    }
+    
+    return queryStringParameterValue;    
+}
